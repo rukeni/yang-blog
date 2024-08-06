e8aab50c463a34eaf309b3ee074204649db8cf78
rukeni <rukeni@naver.com>
2024-08-06 18:56:02 +0900

    🔧 vitest 설정 파일 추가

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: "jsdom",
+    server: {
+      deps: {
+        inline: ["react-tweet"],
+      },
+    },
+  },
+});
